62f5faea1d5526f14c45ca6a18cd768a03469698
Lucas Bombarda <lucas@devdock.com.br>
2024-04-17 07:15:03 -0300

    feat: rocket initial configs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
+use rocket_db_pools::{Connection, Database};
+use repositories::RustaceanRepository;
+
+
 mod models;
 mod repositories;
 mod schema;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Database)]
+#[database("postgres")]
+struct DbConn(rocket_db_pools::diesel::PgPool);
+
+#[rocket::get("/rustaceans")]
+async fn get_rustaceans(mut db: Connection<DbConn>) {
+    RustaceanRepository::find_multiple(&mut db, 100).await.unwrap();
+}
+
+#[rocket::main]
+async fn main() {
+    rocket::build()
+        .mount("/", rocket::routes![get_rustaceans])
+        .attach(DbConn::init())
+        .launch()
+        .await
+        .unwrap();
 }
