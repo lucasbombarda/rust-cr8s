5eac3c1ff6d22e017117fc65f5d366e54c6c23c6
Lucas Bombarda <lucas@devdock.com.br>
2024-04-17 10:24:59 -0300

    format

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use rocket_db_pools::{Connection, Database};
 use repositories::RustaceanRepository;
-
+use rocket_db_pools::{Connection, Database};
 
 mod models;
 mod repositories;
@@ -12,7 +11,9 @@ struct DbConn(rocket_db_pools::diesel::PgPool);
 
 #[rocket::get("/rustaceans")]
 async fn get_rustaceans(mut db: Connection<DbConn>) {
-    RustaceanRepository::find_multiple(&mut db, 100).await.unwrap();
+    RustaceanRepository::find_multiple(&mut db, 100)
+        .await
+        .unwrap();
 }
 
 #[rocket::main]
